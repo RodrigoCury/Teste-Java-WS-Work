e388dd682a69504d390ca3f35708c3593dffe157
= <rodrigocury@ufu.br>
2021-11-10 11:31:14 -0300

    Projeto Base

---
+++ src/main/java/br/dev/rodrigocury/testewswork/TestewsworkApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigocury.testewswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestewsworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestewsworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rodrigocury/testewswork/TestewsworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigocury.testewswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestewsworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
